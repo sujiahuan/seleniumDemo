2163090bb5693e3f29aad6cb9f3c4b769ae44f12
sujiahuan <1059810269@qq.com>
2019-07-08 15:00:27 +0800

    DeBug

--- src/org/jiahuan/Case/LoginTest.java
+++ src/org/jiahuan/Case/LoginTest.java
@@ -11,7 +11,12 @@ public class LoginTest {
 
 	private ChromeDriver driver;
 	
+	@Test
+	public void testName() throws Exception {
+		System.out.println(System.getProperty("user.dir"));
+	}
 
+	@Ignore
 	@Test(description = "登录")
 	public void testLogin() throws Exception {
 		System.out.println(System.getProperty("user.dir"));
